78c5b3dfe144ddb793c59475f2f68a1b41502ddc
bananabread08 <rmkfermin@gmail.com>
2021-05-02 11:58:00 +0800

    files were missing and needed to recommit

--- src/modules/display.js
+++ src/modules/display.js
@@ -1,54 +1,67 @@
-import {projectUL, projectList, LOCAL_STORAGE_PROJECTS_KEY, 
-    LOCAL_STORAGE_CURRENT_PROJECT_ID_KEY,currentProjectID,
-    projectDisplayContainer, projectTitleElement, todoContainer,
-    taskTemplate} from './dom'
+import {
+  projectUL,
+  projectList,
+  LOCAL_STORAGE_PROJECTS_KEY,
+  LOCAL_STORAGE_CURRENT_PROJECT_ID_KEY,
+  currentProjectID,
+  projectDisplayContainer,
+  projectTitleElement,
+  todoContainer,
+  taskTemplate,
+} from './dom';
 
 //module to display stuff
-const render = () => { //render everything
-    clearElement(projectUL);
-    renderProjects();
-    const selectedList = projectList.find(list => list.id.toString() === currentProjectID);
-    if (currentProjectID == null) {
-      projectDisplayContainer.style.display = 'none'
-    } else {
-      projectDisplayContainer.style.display = ''
-      projectTitleElement.innerText = selectedList.name;
-      clearElement(todoContainer);
-      renderTasks(selectedList);
+const render = () => {
+  //render everything
+  clearElement(projectUL);
+  renderProjects();
+  const selectedList = projectList.find(
+    (list) => list.id.toString() === currentProjectID
+  );
+  if (currentProjectID == null) {
+    projectDisplayContainer.style.display = 'none';
+  } else {
+    projectDisplayContainer.style.display = '';
+    projectTitleElement.innerText = selectedList.name;
+    clearElement(todoContainer);
+    renderTasks(selectedList);
+  }
+};
+
+const renderProjects = () => {
+  //render leftNav
+  projectList.forEach((list) => {
+    const listLI = document.createElement('li');
+    listLI.classList.add('project-name');
+    listLI.innerText = list.name;
+    listLI.setAttribute('id', `${list.id}`);
+    if (list.id === currentProjectID) {
+      listLI.classList.add('active-project');
     }
-}
+    projectUL.appendChild(listLI);
+  });
+};
 
-const renderProjects = () => { //render leftNav
-    projectList.forEach(list =>{
-        const listLI = document.createElement("li");
-        listLI.classList.add("project-name");
-        listLI.innerText = list.name;
-        listLI.setAttribute("id", `${list.id}`);
-        if(list.id === currentProjectID) {
-        listLI.classList.add("active-project");
-        }
-        projectUL.appendChild(listLI);
-    });
-}
+const renderTasks = (selectedList) => {
+  //render Todos in specific Project
+  selectedList.todolist.forEach((task) => {
+    const taskElement = document.importNode(taskTemplate.content, true); //Use html Template
+    const label = taskElement.querySelector('label');
+    label.append(`${task.title}, ${task.desc}, ${task.dueDate}`);
+    todoContainer.appendChild(taskElement);
+  });
+};
 
-const renderTasks = (selectedList) => { //render Todos in specific Project
-    selectedList.todolist.forEach(task => {
-      const taskElement = document.importNode(taskTemplate.content, true) //Use html Template
-      const label = taskElement.querySelector('label');
-      label.append(`${task.title}, ${task.desc}, ${task.dueDate}`);
-      todoContainer.appendChild(taskElement)
-    })
-}
-
-const clearElement = (element) => { //clear and replace existing display elements
-    while(element.firstChild){
-        element.removeChild(element.firstChild);
-    }
-}
+const clearElement = (element) => {
+  //clear and replace existing display elements
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+};
 const saveAndDisplay = () => {
-    localStorage.setItem(LOCAL_STORAGE_PROJECTS_KEY,JSON.stringify(projectList));
-    localStorage.setItem(LOCAL_STORAGE_CURRENT_PROJECT_ID_KEY, currentProjectID);
-    render();
-}
+  localStorage.setItem(LOCAL_STORAGE_PROJECTS_KEY, JSON.stringify(projectList));
+  localStorage.setItem(LOCAL_STORAGE_CURRENT_PROJECT_ID_KEY, currentProjectID);
+  render();
+};
 
-export {render, saveAndDisplay} //,showThisProject};
+export { render, saveAndDisplay }; //,showThisProject};
--- src/modules/dom.js
+++ src/modules/dom.js
@@ -1,13 +1,15 @@
 import { Project, Todos } from './constructor';
-import { saveAndDisplay} from './display';
+import { saveAndDisplay } from './display';
 
 //module for DOM stuff
-const projectForm = document.querySelector("#project-form");
-const projectUL = document.querySelector(".project-ul");
-const projectNameInput = document.querySelector("#pname-input")
-const projectDisplayContainer = document.querySelector('[data-project-display-container]'); //content container 
+const projectForm = document.querySelector('#project-form');
+const projectUL = document.querySelector('.project-ul');
+const projectNameInput = document.querySelector('#pname-input');
+const projectDisplayContainer = document.querySelector(
+  '[data-project-display-container]'
+); //content container
 const projectTitleElement = document.querySelector('[data-project-title]'); //project title
-const deleteListButton = document.querySelector('[data-delete-list-button]') //delete project btn
+const deleteListButton = document.querySelector('[data-delete-list-button]'); //delete project btn
 //const listCountElement = document.querySelector('[data-project-count]');
 const todoContainer = document.querySelector('[data-tasks]');
 const taskTemplate = document.getElementById('task-template');
@@ -16,57 +18,76 @@ const newTaskInput = document.querySelector('[data-new-task-input]');
 const newTaskDesc = document.querySelector('[data-new-todo-desc]');
 const newTaskDate = document.querySelector('[data-new-todo-duedate]');
 
-const LOCAL_STORAGE_PROJECTS_KEY = "todo.projectList";
-const LOCAL_STORAGE_CURRENT_PROJECT_ID_KEY = "todo.currentProjectID";
-let projectList = JSON.parse(localStorage.getItem(LOCAL_STORAGE_PROJECTS_KEY)) || [];
-let currentProjectID = localStorage.getItem(LOCAL_STORAGE_CURRENT_PROJECT_ID_KEY); // currentProject
+const LOCAL_STORAGE_PROJECTS_KEY = 'todo.projectList';
+const LOCAL_STORAGE_CURRENT_PROJECT_ID_KEY = 'todo.currentProjectID';
+let projectList =
+  JSON.parse(localStorage.getItem(LOCAL_STORAGE_PROJECTS_KEY)) || [];
+let currentProjectID = localStorage.getItem(
+  LOCAL_STORAGE_CURRENT_PROJECT_ID_KEY
+); // currentProject
+
+let today = new Date().toISOString().split('T')[0];
+document.getElementsByClassName('new date')[0].setAttribute('min', today); //set selectable dates to Today and Future dates only
 
 const addProject = (e) => {
-    e.preventDefault();
-    let projid = 0;
-    for (let count = 0; count <= projectList.length; count++) { //id generator
-        projid++;
-    }
-    const projectName = projectNameInput.value;
-    if (projectName == null || projectName === "") return;
-    const newProject = new Project(projectName, projid);
-    projectList.push(newProject);
-    saveAndDisplay();
-    projectForm.reset();
-}
+  e.preventDefault();
+  let projid = 0;
+  for (let count = 0; count <= projectList.length; count++) {
+    //id generator
+    projid++;
+  }
+  const projectName = projectNameInput.value;
+  if (projectName == null || projectName === '') return;
+  const newProject = new Project(projectName, projid);
+  projectList.push(newProject);
+  saveAndDisplay();
+  projectForm.reset();
+};
 
 const addTodo = (e) => {
-    e.preventDefault();
-    const taskName = newTaskInput.value;
-    const taskDesc = newTaskDesc.value;
-    const taskDate = newTaskDate.value;
-    if (taskName == null || taskName === '') return;
-    const task = new Todos(taskName, taskDesc, taskDate);
-    newTaskInput.value = null;
-    newTaskDesc.value = null;
-    newTaskDate.value = null;
-    const selectedList = projectList.find(list => list.id.toString() === currentProjectID);
-    selectedList.todolist.push(task)
-    saveAndDisplay();
-}
+  e.preventDefault();
+  const taskName = newTaskInput.value;
+  const taskDesc = newTaskDesc.value;
+  const taskDate = newTaskDate.value;
+  if (taskName == null || taskName === '') return;
+  const task = new Todos(taskName, taskDesc, taskDate);
+  newTaskInput.value = null;
+  newTaskDesc.value = null;
+  newTaskDate.value = null;
+  const selectedList = projectList.find(
+    (list) => list.id.toString() === currentProjectID
+  );
+  selectedList.todolist.push(task);
+  saveAndDisplay();
+};
 
-projectUL.addEventListener("click", function (e) { //check current Project
-    if (e.target.tagName.toLowerCase() === "li") {
-        currentProjectID = e.target.id;
-        saveAndDisplay();
-    }
+projectUL.addEventListener('click', function (e) {
+  //check current Project
+  if (e.target.tagName.toLowerCase() === 'li') {
+    currentProjectID = e.target.id;
+    saveAndDisplay();
+  }
 });
 
-projectForm.addEventListener("submit", addProject);
-deleteListButton.addEventListener('click', e => {
-    projectList = projectList.filter(list => list.id.toString() !== currentProjectID)
-    currentProjectID = null;
-    saveAndDisplay();
+projectForm.addEventListener('submit', addProject);
+deleteListButton.addEventListener('click', (e) => {
+  projectList = projectList.filter(
+    (list) => list.id.toString() !== currentProjectID
+  );
+  currentProjectID = null;
+  saveAndDisplay();
 });
 
 newTaskForm.addEventListener('submit', addTodo);
 
-export {projectUL, projectList, LOCAL_STORAGE_PROJECTS_KEY,
-    LOCAL_STORAGE_CURRENT_PROJECT_ID_KEY, currentProjectID,
-    projectDisplayContainer, projectTitleElement, todoContainer,
-    taskTemplate};
+export {
+  projectUL,
+  projectList,
+  LOCAL_STORAGE_PROJECTS_KEY,
+  LOCAL_STORAGE_CURRENT_PROJECT_ID_KEY,
+  currentProjectID,
+  projectDisplayContainer,
+  projectTitleElement,
+  todoContainer,
+  taskTemplate,
+};
