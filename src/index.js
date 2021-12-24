64832bf374af69ae132c8cc43b5c8ebc056bcd69
bananabread08 <rmkfermin@gmail.com>
2021-12-24 16:52:51 +0800

    implemented the functionality to add projects in basic form

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
+import { Project, Todos } from './modules/constructor';
+import uniqid from 'uniqid';
 import './styles/style.css';
+
+const form = document.querySelector('.project-form');
+const projects = [];
+
+const addProject = (e) => {
+  e.preventDefault();
+  const projectName = document.querySelector('.pname-input').value;
+  if (projectName == null || projectName === '') return;
+  const newProject = new Project(projectName, uniqid());
+  projects.push(newProject);
+  form.reset();
+};
+
+form.addEventListener('submit', addProject);
--- src/modules/constructor.js
+++ src/modules/constructor.js
@@ -1,17 +1,33 @@
 //module for Classes
 export class Project {
-    constructor(name, id){
-        this.name = name;
-        this.id = id;
-        this.todolist = []; //JSON.parse(localStorage.getItem(LOCAL_STORAGE_TODOLIST_KEY)) || []; //array of todos
-    }
+  constructor(name, id) {
+    this._name = name;
+    this._id = id;
+    this._todolist = []; //JSON.parse(localStorage.getItem(LOCAL_STORAGE_TODOLIST_KEY)) || []; //array of todos
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(projectName) {
+    return (this._name = projectName);
+  }
+
+  get id() {
+    return this._id;
+  }
+
+  set id(newID) {
+    return (this._id = newID);
+  }
 }
 
 export class Todos {
-    constructor(title, desc, dueDate){
-        this.title = title;
-        this.desc = desc;
-        this.dueDate = dueDate;
-        this.complete = false;
-    }
+  constructor(title, desc, dueDate) {
+    this.title = title;
+    this.desc = desc;
+    this.dueDate = dueDate;
+    this.complete = false;
+  }
 }
