3aa6b5dc602488e70cc3869fb688dfa2e4dc6af5
bananabread08 <rmkfermin@gmail.com>
2021-12-24 17:08:12 +0800

    can now view added project names

--- src/index.js
+++ src/index.js
@@ -3,7 +3,24 @@ import uniqid from 'uniqid';
 import './styles/style.css';
 
 const form = document.querySelector('.project-form');
+
 const projects = [];
+
+const clearChildElements = (element) => {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+};
+
+const renderProjects = (projects) => {
+  const projectsEl = document.querySelector('.project-container');
+  clearChildElements(projectsEl);
+  projects.forEach((project) => {
+    const div = document.createElement('div');
+    div.textContent = `${project.name}`;
+    projectsEl.append(div);
+  });
+};
 
 const addProject = (e) => {
   e.preventDefault();
@@ -11,6 +28,7 @@ const addProject = (e) => {
   if (projectName == null || projectName === '') return;
   const newProject = new Project(projectName, uniqid());
   projects.push(newProject);
+  renderProjects(projects);
   form.reset();
 };
 
