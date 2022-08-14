06e5d764fbb513aaa4ce7b65d24045d58cd7ac2a
bananabread08 <rmkfermin@gmail.com>
2022-08-14 11:37:47 +0800

    create functionality to show todos in current Projects

--- src/index.js
+++ src/index.js
@@ -8,12 +8,16 @@ const todoForm = document.getElementById('todo-form');
 const cancelBtn = document.querySelector('.cancel');
 const projectForm = document.getElementById('project-form');
 const projectsUL = document.querySelector('.projects');
-const deleteBtn = document.querySelectorAll('.deleteBtn');
 let projects = [
   {
     name: 'Test Project',
     id: 'testid',
     todolist: [{ title: 'test todo' }, { title: 'another todo' }],
+  },
+  {
+    name: 'AnotherProject',
+    id: 'testid2',
+    todolist: [{ title: 'asdasdsa' }, { title: 'another' }],
   },
 ];
 
@@ -22,6 +26,10 @@ cancelBtn.addEventListener('click', () => todoModal.close());
 todoForm.addEventListener('submit', () => alert('submitted'));
 projectForm.addEventListener('submit', addProject);
 projectsUL.addEventListener('click', (e) => {
+  if (e.target.nodeName === 'LI')
+    display.renderTodos(
+      projects[projects.findIndex((x) => x.id === e.target.id)]
+    );
   if (e.target.nodeName === 'BUTTON') deleteProject(e);
   return;
 });
@@ -38,6 +46,7 @@ function addProject(e) {
   projects.push(project);
   display.clearElements(projectsUL);
   display.render(projects);
+  display.renderTodos(project);
   projectForm.reset();
   return;
 }
@@ -47,8 +56,10 @@ function deleteProject(e) {
   display.clearElements(projectsUL);
   projects = [...projects].filter((proj) => proj.id !== e.target.parentNode.id);
   display.render(projects);
+  display.renderTodos(projects[0]);
   return;
 }
 
 // init()
 display.render(projects);
+display.renderTodos(projects[0]);
--- src/modules/display.js
+++ src/modules/display.js
@@ -17,21 +17,20 @@ const display = (() => {
       projectLI.textContent = project.name;
       projectLI.appendChild(btn);
       projectsUL.appendChild(projectLI);
-      // for (const todo of project.todolist) {
-      //   const todoLI = document.createElement('li');
-      //   todoLI.textContent = todo.title;
-      //   projectLI.append(todosUL.appendChild(todoLI));
-      // }
     }
     return;
   }
 
   function renderTodos(project) {
+    if (!project) return clearElements(todosUL);
+    const projectLI = document.querySelector(`${project.id}`);
+    clearElements(todosUL);
     for (const todo of project.todolist) {
       const todoLI = document.createElement('li');
       todoLI.textContent = todo.title;
       todosUL.appendChild(todoLI);
     }
+    projectLI?.appendChild(todosUL);
   }
   return { render, clearElements, renderTodos };
 })();
