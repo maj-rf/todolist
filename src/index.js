f64b51ff4246e5a6f1d05603ad2a1d10fb60c99d
bananabread08 <rmkfermin@gmail.com>
2022-08-16 14:39:52 +0800

    deserialize localStorage todos into local classes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 import './styles/style.css';
 import Project from './modules/Projects';
 import init from './modules/init';
+import Todos from './modules/Todos';
 
 const storage = JSON.parse(localStorage.getItem('projects')) || [];
 let projects = storage.map(
-  (proj) => new Project(proj._name, proj._id, proj._todolist)
+  (proj) =>
+    new Project(
+      proj?._name,
+      proj?._id,
+      proj?._todolist.map(
+        (todo) =>
+          new Todos(
+            todo?._title,
+            todo?._details,
+            todo?._due,
+            todo?._priority,
+            todo?._id
+          )
+      )
+    )
 ); //deserialize JSON => convert to local class
+
+console.log(projects);
 let currentProject = projects[0];
 
 // init()
