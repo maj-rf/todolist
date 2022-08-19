d506ba131cb02d86677470cc5b628a04171bb35d
bananabread08 <rmkfermin@gmail.com>
2022-08-19 21:22:19 +0800

    finish all UI styling

--- src/index.js
+++ src/index.js
@@ -3,7 +3,44 @@ import Project from './modules/Projects';
 import init from './modules/init';
 import Todos from './modules/Todos';
 
-const storage = JSON.parse(localStorage.getItem('projects')) || [];
+// [new Project('Study Finals', 'proj1', [new Todos('Calculus', 'Read Chapter 7', '2024-08-20', 'critical', true)])]
+
+const storage = JSON.parse(localStorage.getItem('projects')) || [
+  new Project('Study Finals', 'proj1', [
+    new Todos(
+      'Calculus',
+      'Read Chapter 7',
+      '2024-08-20',
+      'critical',
+      'todo1',
+      false
+    ),
+    new Todos(
+      'Click Title to Show Details',
+      'These are the details of this todo',
+      '2024-08-21',
+      'normal',
+      'todo2',
+      false
+    ),
+    new Todos(
+      'This task is done',
+      'Click the circle button to toggle',
+      '2024-08-22',
+      'important',
+      'todo3',
+      true
+    ),
+    new Todos(
+      'DeleteMe',
+      'Click the trash icon to delete this todo',
+      '2024-08-23',
+      'low',
+      'todo4',
+      false
+    ),
+  ]),
+];
 let projects = storage.map(
   (proj) =>
     new Project(
